c96f6ffebc3904485d79fe96305b265b6cfb18fe
Doan Mong <ddmong@cpp.edu>
2022-10-07 21:26:50 -0700

    updated board

--- tictactoe.c
+++ tictactoe.c
@@ -1,9 +1,15 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
 
-char box[10]={'0','1','2','3','4','5','6','7','8','9'};
-void creating_board();
+char box[3][3];     
+int counter = 0;
+
+void random_move(void);
+void creating_board(void);
 void marking_board(int, char);
-int check_winner();
+int check_available(char c, int player);
+void check_winner(void);
 
 int main() {
     creating_board();
@@ -13,11 +19,11 @@ int main() {
 void creating_board() {
     printf("The current status is:\n\n");
     printf("+------------+\n");
-    printf("| %c | %c | %c |\n",box[1],box[2],box[3]);
+    printf("| %c | %c | %c |\n",box[0][0],box[0][1],box[0][2]);
     printf("+------------+\n");
-    printf("| %c | %c | %c |\n",box[4],box[5],box[6]);
+    printf("| %c | %c | %c |\n",box[1][0],box[1][1],box[1][2]);
     printf("+------------+\n");
-    printf("| %c | %c | %c |\n",box[7],box[8],box[9]);
+    printf("| %c | %c | %c |\n",box[2][0],box[2][1],box[2][2]);
     printf("+------------+\n");
 
 }
